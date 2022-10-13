2bf3a4f9ffbc8f68465ab7f259d65bc2bb4835fb
bogdanmosica <bogdan.mosica@gmail.com>
2022-10-13 14:49:20 +0300

    removed unnecessary packages

--- readFile.js
+++ readFile.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-
 const json2md = require("json2md");
 const { parser } = require("html-metadata-parser");
 
@@ -40,89 +39,93 @@ const readFile = () => {
 			.map(async (url, index) => {
 				try {
 					const result = await parser(url);
+					if (index === 0) {
+						const thedata = await parser("https://carrd.co/");
+						console.log(thedata);
+					}
 					return result;
 				} catch (error) {
 					return {};
 				}
 			});
 
-		Promise.all(promiseData).then(function (values) {
-			const appList = values.map(({ og, meta, images }, i) => ({
-				...og,
-				...meta,
-				...images,
-				...extractedData[i],
-			}));
-			const markdownModelContent = filteredH1Headers.reduce(
-				(prev, current, index) => {
-					const linkName = current.split("#")[1].slice(1);
-					prev.push({
-						link: {
-							title: `${index + 1}. ${linkName}`,
-							source: `#${linkName.replace(/\s+/g, "-").toLowerCase()}`,
-						},
-					});
-					return prev;
-				},
-				[
-					{
-						h1: "Internet Tools Collection",
-					},
-					{
-						p: "A collection of tools, website and AI for entrepreneurs, web designers, programmers and for everyone else.",
-					},
-					{
-						h1: "Content by category",
-					},
-				]
-			);
-			const markdownModel = appList.reduce(
-				(
-					prev,
-					{
-						id,
-						url,
-						name,
-						heading,
-						title = "",
-						image = "",
-						images = [],
-						description = "",
-					}
-				) => {
-					if (id === 1)
-						prev.push({
-							[textHeaders.includes(heading) ? "h1" : "h2"]: heading,
-						});
-					const appTitle = {
-						h2: `[${id}. ${title || name}](${url})`,
-					};
-					const appIcon = {
-						link: {
-							title: `<img src='${
-								image || images.length > 0
-									? images[0]?.src
-									: "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
-							}' width="300" />`,
-							source: url,
-						},
-					};
-					const appDescription = {
-						blockquote: description,
-					};
-					// const appKeywords = {
-					// 	p: `Keywords: ${keywords}`,
-					// };
-					const breakLine = {
-						p: "---",
-					};
-					prev.push(appTitle, appIcon, appDescription, breakLine);
-					return prev;
-				},
-				markdownModelContent
-			);
-			fs.writeFileSync("./readme.md", json2md(markdownModel));
-		});
+		// Promise.all(promiseData).then(function (values) {
+		// 	const appList = values.map(({ og, meta, images }, i) => ({
+		// 		...og,
+		// 		...meta,
+		// 		...images,
+		// 		...extractedData[i],
+		// 	}));
+		// 	const markdownModelContent = filteredH1Headers.reduce(
+		// 		(prev, current, index) => {
+		// 			const linkName = current.split("#")[1].slice(1);
+		// 			prev.push({
+		// 				link: {
+		// 					title: `${index + 1}. ${linkName}`,
+		// 					source: `#${linkName.replace(/\s+/g, "-").toLowerCase()}`,
+		// 				},
+		// 			});
+		// 			return prev;
+		// 		},
+		// 		[
+		// 			{
+		// 				h1: "Internet Tools Collection",
+		// 			},
+		// 			{
+		// 				p: "A collection of tools, website and AI for entrepreneurs, web designers, programmers and for everyone else.",
+		// 			},
+		// 			{
+		// 				h1: "Content by category",
+		// 			},
+		// 		]
+		// 	);
+		// 	const markdownModel = appList.reduce(
+		// 		(
+		// 			prev,
+		// 			{
+		// 				id,
+		// 				url,
+		// 				name,
+		// 				heading,
+		// 				title = "",
+		// 				image = "",
+		// 				images = [],
+		// 				description = "",
+		// 			}
+		// 		) => {
+		// 			if (id === 1)
+		// 				prev.push({
+		// 					[textHeaders.includes(heading) ? "h1" : "h2"]: heading,
+		// 				});
+		// 			const appTitle = {
+		// 				h2: `[${id}. ${title || name}](${url})`,
+		// 			};
+		// 			const appIcon = {
+		// 				link: {
+		// 					title: `<img src='${
+		// 						image || images.length > 0
+		// 							? images[0]?.src
+		// 							: "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
+		// 					}' width="300" />`,
+		// 					source: url,
+		// 				},
+		// 			};
+		// 			const appDescription = {
+		// 				blockquote: description,
+		// 			};
+		// 			// const appKeywords = {
+		// 			// 	p: `Keywords: ${keywords}`,
+		// 			// };
+		// 			const breakLine = {
+		// 				p: "---",
+		// 			};
+		// 			prev.push(appTitle, appIcon, appDescription, breakLine);
+		// 			return prev;
+		// 		},
+		// 		markdownModelContent
+		// 	);
+		// 	fs.writeFileSync("./readme.md", json2md(markdownModel));
+		// });
 	});
 };
 module.exports = readFile;
